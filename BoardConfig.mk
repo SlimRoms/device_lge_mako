711901908f11a536d36578a239d695a55f2b5359
Lars Greiss <kufikugel@googlemail.com>
2013-09-05 20:29:12 +0200

    Mako: Set for JWR kernel
    Change-Id: I721d5a829db48c33cb613a2cd3a6c3412a1420f2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,7 @@
 #
 
 TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp -DTARGET_MAKO_JWR66
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
