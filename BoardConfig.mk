CyanogenMod/android_device_lge_mako

mako: Specify the socket name for PowerHAL
 * Current code in AOSP points to a new location. The socket name is
   hard coded in MPDecision, so let's compensate for now.
 * We can revert this when Google releases an update that brings it in
   line with Flo.

Change-Id: I893d06a7c6b67c408c2f74ae064a2544c4d495d4

BoardConfig.mk
@@ -30,6 +30,7 @@ TARGET_KRAIT_BIONIC_PLDOFFS := 10
 TARGET_KRAIT_BIONIC_PLDTHRESH := 10
 TARGET_KRAIT_BIONIC_BBTHRESH := 64
 TARGET_KRAIT_BIONIC_PLDSIZE := 64
+TARGET_MPDECISION_BOOST_SOCKET := /dev/socket/mpdecision/boost
 
 TARGET_NO_BOOTLOADER := true
 

SlimRoms/device_lge_mako

Conflicts:
	BoardConfig.mk

BoardConfig.mk
@@ -23,6 +23,8 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := krait
 
+TARGET_MPDECISION_BOOST_SOCKET := /dev/socket/mpdecision/touchboost
+
 TARGET_NO_BOOTLOADER := true
 
 TARGET_PREBUILT_KERNEL := device/lge/mako-kernel/kernel