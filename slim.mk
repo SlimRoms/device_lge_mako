44d8b1e2d86aaf73003718c8528c5fa198bf3d18
Tommi Finnilae <baijeri@gmail.com>
2014-04-21 00:33:37 +0300

    slim.mk: Reorganize makefile to fix incorrect dataroaming flag
    Calling subsequent .mk files in a wrong order caused the dataroaming flag being set incorrectly.
    
    Layout matched with Hammerhead one atm.
    
    Change-Id: I64666420f3f77b789acf5fecbf5a45e3310f838c

--- slim.mk
+++ slim.mk
@@ -2,17 +2,17 @@
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit device configuration
-$(call inherit-product, device/lge/mako/full_mako.mk)
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/slim/config/gsm.mk)
 
 # Inherit some common Slim stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/slim/config/gsm.mk)
-
 # Enhanced NFC
 $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/mako/full_mako.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := mako
@@ -26,4 +26,3 @@ PRODUCT_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT="google/occam/mako:4.4.2/KOT49H/937116:user/release-keys" PRIVATE_BUILD_DESC="occam-user 4.4.2 KOT49H 937116 release-keys"
-
