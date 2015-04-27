9ecc576ae645acabe701f9450726154b35d10b58
Josue Rivera <prbassplayer@gmail.com>
2015-04-27 09:15:47 -0600

    Mako: God damnit Google -_-'
      * Fix mako barriers
    
    Change-Id: I94cdaca6fba52e0218c6ff5909de5300da4ac716
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- Android.mk
+++ Android.mk
@@ -43,6 +43,6 @@ LOCAL_SRC_FILES    := $(LOCAL_MODULE)
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
 include $(BUILD_PREBUILT)
 
-endif
-
 include $(call first-makefiles-under,$(LOCAL_PATH))
+
+endif
