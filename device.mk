12705eb09657726eb8b34170868424f6027e2948
Josue Rivera <prbassplayer@gmail.com>
2015-06-24 21:12:56 -0400

    Disable Showp's MpDecision by default && clean up spaces 	Thanks Mercado_Negro for the idea
    Change-Id: Ifcdbb9cff212d730c36698a5bb5edf88e935bcbc
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- device.mk
+++ device.mk
@@ -116,6 +116,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     device/lge/mako/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf
 
+# Add init.d file to disable stock mpdecision
+PRODUCT_COPY_FILES += \
+	device/lge/mako/01mpdecision:system/etc/init.d/01mpdecision
+
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.opengles.version=196608
 
