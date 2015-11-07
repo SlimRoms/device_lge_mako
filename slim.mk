8ea554517b0158871d11241550075b622d2a5942
Josue Rivera <prbassplayer@gmail.com>
2015-11-07 19:14:11 +0100

    mako: Update fingerprint to LMY48T
    Change-Id: Iccb0eb13eb9bb108e573503f0cabf5bfcaca40a2
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- slim.mk
+++ slim.mk
@@ -26,5 +26,5 @@ PRODUCT_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-   PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:5.1.1/LMY48M/2167285:user/release-keys \
-   PRIVATE_BUILD_DESC="occam-user 5.1.1 LMY48M 2167285 release-keys"
+   PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:5.1.1/LMY48T/2237560:user/release-keys \
+   PRIVATE_BUILD_DESC="occam-user 5.1.1 LMY48T 2237560 release-keys"
